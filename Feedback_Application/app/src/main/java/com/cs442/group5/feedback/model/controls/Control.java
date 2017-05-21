78b56153b7ecba3cbdd5d36b6e0b7c688c8204b7
Shivani Sanjay Shinde <shindeshivani26@gmail.com>
2017-05-21 12:19:47 -0500

    Update Control.java

--- Feedback_Application/app/src/main/java/com/cs442/group5/feedback/model/controls/Control.java
+++ Feedback_Application/app/src/main/java/com/cs442/group5/feedback/model/controls/Control.java
@@ -1,9 +1,5 @@
 package com.cs442.group5.feedback.model.controls;
 
-/**
- * Created by sauja7 on 11/1/16.
- */
-
 public class Control {
 
 	public static final String RADIO="RADIO";
@@ -11,8 +7,5 @@ public class Control {
 	public static final String DROPDOWN="DROPDOWN";
 	public static final String TEXT="TEXT";
 	public static final String RATING="RATING";
-
-
-
 
 }
